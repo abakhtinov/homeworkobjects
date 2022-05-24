95e49848be46f94b2505b9411064b3dab2b5b459
aleksandr <abakhtinov.nsk@gmail.com>
2022-05-24 11:29:37 +0300

    середина

--- src/homework10/Book.java
+++ src/homework10/Book.java
@@ -4,10 +4,14 @@ public class Book {
     private String namebook;
     private int publicationYear;
 
+    private Author author;
 
-    public Book(String namebook, int publicationYear) {
+
+    public Book (String namebook, Author author, int publicationYear) {
         this.namebook = namebook;
+        this.author = author;
         this.publicationYear = publicationYear;
+
     }
 
     public String getNamebook() {
@@ -16,6 +20,10 @@ public class Book {
 
     public int getPublicationYear() {
         return this.publicationYear;
+    }
+
+    public Author getAuthor() {
+        return this.author;
     }
 
     public void setPublicationYear(int publicationYear) {
--- src/homework10/Lesson10.java
+++ src/homework10/Lesson10.java
@@ -9,19 +9,17 @@ public class Lesson10 {
     public static void main(String[] args) {
 
         Author tolkin = new Author("Джон", "Толкин");
-        System.out.println("tolkin.surname = " + tolkin.getSurname());
-        System.out.println("tolkin.name = " + tolkin.getName());
 
         Author joanne = new Author("Джоан", "Роулинг");
-        System.out.println("joanne.surname = " + joanne.getSurname());
-        System.out.println("joanne.name = " + joanne.getName());
 
-        Book lor = new Book("Властелин колец", 1954);
+        Book lor = new Book("Властелин колец", tolkin, 1954 );
+        System.out.println("lor.getAuthor() = " + lor.getAuthor().getName() + " " + lor.getAuthor().getSurname());
         System.out.println("lor.name = " + lor.getNamebook());
         System.out.println("lor.publicationYear = " + lor.getPublicationYear());
-        
 
-        Book gp = new Book("Гарри Поттер", 1997);
+
+        Book gp = new Book("Гарри Поттер", joanne ,1997);
+        System.out.println("gp.getAuthor() = " + gp.getAuthor().getName() + " " + gp.getAuthor().getSurname());
         System.out.println("gp.namebook = " + gp.getNamebook());
         System.out.println("gp.publicationYear = " + gp.getPublicationYear());
         gp.setPublicationYear(1998);
